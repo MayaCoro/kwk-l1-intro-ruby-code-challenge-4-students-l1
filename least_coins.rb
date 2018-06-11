25453538255433ece833db21abe5e9c414ac6662
MayaCoro <mayascoro@gmail.com>
2018-06-11 05:41:40 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,7 +1,13 @@
 #write out your code here
 
-def least_coins(cents)
-
+def least_coins(cents) = {
+  :quarters => 25,
+  :dimes => 10,
+  :nickles => 2
+  :pennies => 1
+}
 #Code your answer here!
 
 end
+
+puts least_coins(29)
